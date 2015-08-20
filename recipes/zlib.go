3e48bd5368e9ff46cf38ee8fb60c23e852556e0b
Andrew Dunham <andrew@du.nham.ca>
2015-08-19 23:11:35 -0700

    Fix zlib when cross-compiling to darwin

--- recipes/zlib.go
+++ recipes/zlib.go
@@ -1,8 +1,10 @@
 package recipes
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 
 	"github.com/andrew-d/sbuild/builder"
 	"github.com/andrew-d/sbuild/recipes/templates"
@@ -35,6 +37,7 @@ func (r *ZlibRecipe) Build(ctx *types.BuildContext) error {
 	log.Info("Building zlib")
 	srcdir := r.UnpackedDir(ctx, r.Info())
 
+	// 1. Configure
 	cmd := exec.Command("./configure", "--static")
 	cmd.Dir = srcdir
 	cmd.Env = ctx.Env.
@@ -50,6 +53,24 @@ func (r *ZlibRecipe) Build(ctx *types.BuildContext) error {
 		return err
 	}
 
+	// 2. Fix path to libtool when cross-compiling to Darwin
+	if ctx.Platform == "darwin" {
+		cmd = exec.Command(
+			"sed",
+			"-i",
+			"-e",
+			fmt.Sprintf("s|AR=/usr/bin/libtool|AR=%s-ar|g", ctx.CrossPrefix),
+			"-e",
+			"s|ARFLAGS=-o|ARFLAGS=rc|g",
+			filepath.Join(srcdir, "Makefile"),
+		)
+		if err := cmd.Run(); err != nil {
+			log.WithField("err", err).Error("Could not patch Makefile")
+			return err
+		}
+	}
+
+	// 3. Run build
 	cmd = exec.Command("make")
 	cmd.Dir = srcdir
 	cmd.Stdout = os.Stdout
